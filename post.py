b7ab32dba4847507f466aea3cb3465a3f592b212
IDeployOnFriday <146853229+IDeployOnFriday@users.noreply.github.com>
2023-12-06 21:17:46 +0000

    using detail api

--- post.py
+++ post.py
@@ -8,6 +8,13 @@ from requests.auth import HTTPBasicAuth
 
 import auth
 
+#  get info about station
 res = requests.get('https://api.rtt.io/api/v1/json/search/BGN', auth=HTTPBasicAuth(auth.Username, auth.Password))
 result = json.loads(res.content)
-print(result['location'])
+print(result)
+
+# .....
+#  get info on specific service
+res = requests.get('https://api.rtt.io/api/v1/json/service/H06145/2023/12/06', auth=HTTPBasicAuth(auth.Username, auth.Password))
+result = json.loads(res.content)
+print(result)
