341706b382dc75e0c25b2669d32bb32a0a211a0f
IDeployOnFriday <146853229+IDeployOnFriday@users.noreply.github.com>
2023-12-07 17:59:25 +0000

    using python lib

--- post.py
+++ post.py
@@ -7,14 +7,27 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 import auth
+from rttapi.api import RttApi
 
-#  get info about station
-res = requests.get('https://api.rtt.io/api/v1/json/search/BGN', auth=HTTPBasicAuth(auth.Username, auth.Password))
-result = json.loads(res.content)
-print(result)
 
-# .....
-#  get info on specific service
-res = requests.get('https://api.rtt.io/api/v1/json/service/H06145/2023/12/06', auth=HTTPBasicAuth(auth.Username, auth.Password))
-result = json.loads(res.content)
-print(result)
+
+def lib_request():
+    api = RttApi(auth.Username, auth.Password)
+    departures = api.search_station_departures('CLJ')
+    print(departures.services)
+
+
+def curl_request():
+    #  get info about station
+    res = requests.get('https://api.rtt.io/api/v1/json/search/BGN', auth=HTTPBasicAuth(auth.Username, auth.Password))
+    result = json.loads(res.content)
+    print(result)
+
+    # .....
+    #  get info on specific service
+    res = requests.get('https://api.rtt.io/api/v1/json/service/H06145/2023/12/06', auth=HTTPBasicAuth(auth.Username, auth.Password))
+    result = json.loads(res.content)
+    print(result)
+
+if __name__ == '__main__':
+    lib_request()
