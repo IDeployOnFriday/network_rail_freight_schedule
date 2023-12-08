d8090ad10a58d70c9291a5ca2abc1e3cbf893a7c
IDeployOnFriday <146853229+IDeployOnFriday@users.noreply.github.com>
2023-12-08 22:24:08 +0000

    getting json data

--- post.py
+++ post.py
@@ -9,13 +9,15 @@ from requests.auth import HTTPBasicAuth
 import auth
 from rttapi.api import RttApi
 
-
+freight_url = 'https://publicdatafeeds.networkrail.co.uk/ntrod/CifFileAuthenticate?type=CIF_FREIGHT_FULL_DAILY&day=toc-full'
 
 def lib_request():
     api = RttApi(auth.Username, auth.Password)
     departures = api.search_station_departures('CLJ')
     print(departures.services)
 
+def get_freight_timetable():
+    res = requests.get(freight_url,  auth=HTTPBasicAuth(auth.feed_username, auth.feed_password))
 
 def curl_request():
     #  get info about station
@@ -25,9 +27,9 @@ def curl_request():
 
     # .....
     #  get info on specific service
-    res = requests.get('https://api.rtt.io/api/v1/json/service/H06145/2023/12/06', auth=HTTPBasicAuth(auth.Username, auth.Password))
+    res = requests.get('https://api.rtt.io/api/v1/json/service/H31618/2023/12/07', auth=HTTPBasicAuth(auth.Username, auth.Password))
     result = json.loads(res.content)
     print(result)
 
 if __name__ == '__main__':
-    lib_request()
+    get_freight_timetable()
