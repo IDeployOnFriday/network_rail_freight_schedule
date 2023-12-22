e88b8ebd66b616b25e35d0a7e03c76709b6f32e5
IDeployOnFriday <146853229+IDeployOnFriday@users.noreply.github.com>
2023-12-22 17:54:13 +0000

    formatting update

--- format_output.py
+++ format_output.py
@@ -1,9 +1,15 @@
 import re
 
+from dates import is_date_between
+
+
 def get_timetable(trains_passing):
     ordered_trains = []
     for train in trains_passing:
         uid = train['JsonScheduleV1']['CIF_train_uid']
+        schedule_end_date = train['JsonScheduleV1']['schedule_end_date']
+        schedule_start_date = train['JsonScheduleV1']['schedule_start_date']
+
         schedule_locations = train['JsonScheduleV1']['schedule_segment']['schedule_location']
         for stop in schedule_locations:
             place = stop['tiploc_code']
@@ -11,11 +17,11 @@ def get_timetable(trains_passing):
                 try:
                     time = re.sub("[^0-9]", "", stop['pass'])
                     print('{} Passing {} : {}'.format(uid, place, time))
-                    ordered_trains.append([uid, place, time])
+                    my_date='2023-12-22'
+                    if is_date_between(schedule_start_date, schedule_end_date,my_date):
+                        ordered_trains.append([uid, place, time,schedule_start_date,schedule_end_date])
                 except:
                     print("An exception occurred")
-
-    tmp = [('a', 'b', '011'),('a', 'b', '120'),('a', 'b', '015')]
 
     sorted_by_second = sorted(ordered_trains, key=lambda tup: tup[2])
     for t in sorted_by_second:
