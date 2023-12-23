9228f0923787f662ac0d63d12cde2bd3d930a178
IDeployOnFriday <146853229+IDeployOnFriday@users.noreply.github.com>
2023-12-23 23:49:10 +0000

    check if new schedule available

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from format_output import get_timetable
 from get_station_timetable import get_trains_passing_today
-from schedule_helper import get_freight_timetable, read_schedule
+from schedule_helper import get_freight_timetable, is_new_timeTable, read_schedule
 from preprocess_files import split_files
 
 
@@ -12,9 +12,12 @@ BRGEND
 
 
 if __name__ == '__main__':
-    # get_freight_timetable()
-    # split_files()
-    trains_passing = read_schedule('BRGEND')
+
+    if is_new_timeTable():
+        get_freight_timetable()
+        split_files()
+
+    trains_passing = read_schedule('CDF')
     (running_trains, not_running_trains) = get_trains_passing_today(trains_passing)
 
     print('Number of trains running {} '.format(len(running_trains)))
--- schedule_helper.py
+++ schedule_helper.py
@@ -2,9 +2,35 @@ import gzip
 import json
 import shutil
 import subprocess
+from datetime import datetime
+
 import auth
 
 freight_url = 'https://publicdatafeeds.networkrail.co.uk/ntrod/CifFileAuthenticate?type=CIF_FREIGHT_FULL_DAILY&day=toc-full'
+
+
+def is_new_timeTable(): 
+     
+    with open('schedule.txt') as f:
+        first_line = f.readline().strip('\n')
+        schedule_meta_data = json.loads(first_line)
+        timestamp = schedule_meta_data['JsonTimetableV1']['timestamp']
+        dt_object = datetime.fromtimestamp(timestamp)
+
+        today = datetime.today()
+        d1 = today.strftime("%d/%m/%Y")
+        d2 = dt_object.strftime("%d/%m/%Y")
+        print(d1, d2)
+        if d1 == d2:
+            return False
+        else:
+            return True
+
+
+
+
+
+
 def get_freight_timetable():
     # get the freight timetable using curl
     curl_string = "curl -L -u '{}:{}' -o file.gz '{}'".format(auth.feed_username, auth.feed_password, freight_url)
