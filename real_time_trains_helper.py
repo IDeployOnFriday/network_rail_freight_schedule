6e9238ab607a1a229cb1b2fadf9710d17a69b9f4
IDeployOnFriday <146853229+IDeployOnFriday@users.noreply.github.com>
2023-12-24 19:07:15 +0000

    getting canelled trains from html page

--- real_time_trains_helper.py
+++ real_time_trains_helper.py
@@ -1,3 +1,12 @@
+import json
+from datetime import datetime
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+import auth
+
+
 def lib_request():
     api = RttApi(auth.Username, auth.Password)
     departures = api.search_station_departures('CLJ')
@@ -26,5 +35,21 @@ def get_service_info_today(service):
 
     res = requests.get('https://api.rtt.io/api/v1/json/service/{}/{}/{}/{}'.format(service, year, month, day),
                        auth=HTTPBasicAuth(auth.Username, auth.Password))
+    print('https://api.rtt.io/api/v1/json/service/{}/{}/{}/{}'.format(service, year, month, day))
     result = json.loads(res.content)
     print(result)
+
+def latest_info_on_train(service):
+    today = datetime.today()
+    d1 = today.strftime("%d/%m/%Y")
+    day = today.strftime('%d')
+    month = today.strftime('%m')
+    year = today.strftime('%Y')
+
+    res = requests.get('https://www.realtimetrains.co.uk/service/gb-nr:{}/{}-{}-{}/detailed'.format(service, year, month, day),
+                       auth=HTTPBasicAuth(auth.Username, auth.Password))
+    print('https://www.realtimetrains.co.uk/service/gb-nr:{}/{}-{}-{}/detailed'.format(service, year, month, day))
+
+
+if __name__ == '__main__':
+    latest_info_on_train('H02140')
