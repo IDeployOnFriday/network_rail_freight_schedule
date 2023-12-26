9a112447f76924c60ef69112536ef56f004d9f6e
IDeployOnFriday <146853229+IDeployOnFriday@users.noreply.github.com>
2023-12-26 21:07:19 +0000

    config extracted

---
+++ config.py
@@ -0,0 +1,8 @@
+freight_url = 'https://publicdatafeeds.networkrail.co.uk/ntrod/CifFileAuthenticate?type=CIF_FREIGHT_FULL_DAILY&day=toc-full'
+workingDir = 'workingDir'
+station = 'CHANGEME'
+
+JsonAssociation = 'JsonAssociation.json'
+JsonSchedule = 'JsonSchedule.json'
+schedule = 'schedule.json'
+tiploc = 'tiploc.json'
